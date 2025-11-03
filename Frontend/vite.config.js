395b169385e7355af07d83330289a55758d87447
bhanu-3333 <bhanuu339@gmail.com>
2025-11-03 20:00:50 +0530

    vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000/'
-      }
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/',
+      },
+    },
   },
   plugins: [react()],
 })
