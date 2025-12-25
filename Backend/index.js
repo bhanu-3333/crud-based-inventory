58661683c1ad91afaf97664c11600b1ba4d51f27
bhanu-3333 <bhanuu339@gmail.com>
2025-12-25 22:26:09 +0530

    moongose exchanged

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,6 @@ server.get("/", (req, res) => {
 server.get("*", (req, res) => {
   res.status(404).send("404 NOT FOUND <a href='./'> Go To Home</a>");
 });
-
 // ✅ Connect to MongoDB and start the server
 connectDB()
   .then(() => {
