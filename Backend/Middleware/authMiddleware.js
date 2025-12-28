9f84b15a45911a7f52d15acf1234bbeeb54abd40
bhanu-3333 <bhanuu339@gmail.com>
2025-12-28 21:41:20 +0530

    auth route changed

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -5,7 +5,6 @@ const authMiddleware = (req, res, next) => {
     if (!token) {
       return res.status(401).json({ status:false,message: 'Unauthorized' });
     }
-  
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
       // console.log("token decoded::",decoded);
